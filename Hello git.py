8b16f9392f85b157ff73de67f9bf3464bedb223c
Jaaime03 <95310242+Jaaime03@users.noreply.github.com>
2023-09-23 19:10:13 +0200

    Esto sirve para sumar

---
+++ Hello git.py
@@ -0,0 +1,9 @@
+# Pedir al usuario que ingrese dos números
+numero1 = float(input("Ingresa el primer número: "))
+numero2 = float(input("Ingresa el segundo número: "))
+
+# Realizar la suma
+suma = numero1 + numero2
+
+# Mostrar el resultado
+print("La suma de {} y {} es igual a {}".format(numero1, numero2, suma))
