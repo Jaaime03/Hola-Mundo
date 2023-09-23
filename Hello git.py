72fb03ef80cf63efc2c7eb78ebc8dd732f64ee2e
Jaaime03 <95310242+Jaaime03@users.noreply.github.com>
2023-09-23 19:13:20 +0200

    Hemos implementado la multiplicación

--- Hello git.py
+++ Hello git.py
@@ -4,6 +4,8 @@ numero2 = float(input("Ingresa el segundo número: "))
 
 # Realizar la suma
 suma = numero1 + numero2
+multi = numero1*numero2
 
 # Mostrar el resultado
 print("La suma de {} y {} es igual a {}".format(numero1, numero2, suma))
+print("La multiplicación de {} y {} es igual a {}".format(numero1, numero2, multi))
